5dadc0ba86271113030ceba3b9f02bc5c633c0a4
MRSa <mrsa@myad.jp>
2024-08-02 00:40:25 +0900

    Home画面作成中。

--- app/src/main/java/jp/osdn/gokigen/aira01c/MainActivity.kt
+++ app/src/main/java/jp/osdn/gokigen/aira01c/MainActivity.kt
@@ -1,35 +1,171 @@
 package jp.osdn.gokigen.aira01c
 
+import android.Manifest.permission
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import jp.osdn.gokigen.aira01c.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity()
+{
     private lateinit var binding: ActivityMainBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+        try
+        {
+            val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_tips
+            val navController = findNavController(R.id.nav_host_fragment_activity_main)
+            val appBarConfiguration = AppBarConfiguration(
+                setOf(
+                    R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_tips
+                )
             )
+            setupActionBarWithNavController(navController, appBarConfiguration)
+            navView.setupWithNavController(navController)
+        }
+        catch (e: Exception)
+        {
+            e.printStackTrace()
+        }
+
+        //setupNavigation()
+
+/*
+        try
+        {
+            setupNavigation()
+
+            ///////// SET PERMISSIONS /////////
+            Log.v(TAG, " ----- SET PERMISSIONS -----")
+            if (!allPermissionsGranted())
+            {
+                val requestPermission = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+
+                    ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_NEED_PERMISSIONS)
+                    if(!allPermissionsGranted())
+                    {
+                        // Abort launch application because required permissions was rejected.
+                        Toast.makeText(this, getString(R.string.permission_not_granted), Toast.LENGTH_SHORT).show()
+                        Log.v(TAG, "----- APPLICATION LAUNCH ABORTED -----")
+                        finish()
+                    }
+                }
+                requestPermission.launch(REQUIRED_PERMISSIONS)
+            }
+        }
+        catch (e: Exception)
+        {
+            e.printStackTrace()
+        }
+
+        setupNavigation()
+*/
+    }
+
+    private fun setupNavigation()
+    {
+        try
+        {
+            val navView: BottomNavigationView = binding.navView
+
+            val navController = findNavController(R.id.nav_host_fragment_activity_main)
+            val appBarConfiguration = AppBarConfiguration(
+                setOf(
+                    R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_tips
+                )
+            )
+            setupActionBarWithNavController(navController, appBarConfiguration)
+            navView.setupWithNavController(navController)
+        }
+        catch (e: Exception)
+        {
+            e.printStackTrace()
+        }
+    }
+
+    private fun allPermissionsGranted() : Boolean
+    {
+        var result = true
+        for (param in REQUIRED_PERMISSIONS)
+        {
+            if (ContextCompat.checkSelfPermission(
+                    baseContext,
+                    param
+                ) != PackageManager.PERMISSION_GRANTED
+            )
+            {
+                // ----- Permission Denied...
+                if ((param == permission.ACCESS_MEDIA_LOCATION)&&(Build.VERSION.SDK_INT < Build.VERSION_CODES.Q))
+                {
+                    //　この場合は権限付与の判断を除外 (デバイスが (10) よりも古く、ACCESS_MEDIA_LOCATION がない場合）
+                }
+                else
+                {
+                    // ----- 権限が得られなかった場合...
+                    Log.v(TAG, " Permission: $param : ${Build.VERSION.SDK_INT}")
+                    result = false
+                }
+            }
+        }
+        return (result)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray)
+    {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        Log.v(TAG, "------------------------- onRequestPermissionsResult() ")
+        if (requestCode == REQUEST_NEED_PERMISSIONS)
+        {
+            if (allPermissionsGranted())
+            {
+                // ----- 権限が有効だった
+                Log.v(TAG, "onRequestPermissionsResult()")
+                setupNavigation()
+            }
+            else
+            {
+                // ----- 許可されない権限があった。終了する。
+                Log.v(TAG, "----- onRequestPermissionsResult() : false")
+                Toast.makeText(this, getString(R.string.permission_not_granted), Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    companion object
+    {
+        private val TAG = MainActivity::class.java.simpleName
+        private const val REQUEST_NEED_PERMISSIONS = 1010
+
+        @SuppressLint("InlinedApi")
+        private val REQUIRED_PERMISSIONS = arrayOf(
+            permission.INTERNET,
+            permission.VIBRATE,
+            permission.ACCESS_MEDIA_LOCATION,
+            permission.ACCESS_NETWORK_STATE,
+            permission.ACCESS_WIFI_STATE,
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
     }
 }
--- app/src/main/java/jp/osdn/gokigen/aira01c/ui/home/HomeFragment.kt
+++ app/src/main/java/jp/osdn/gokigen/aira01c/ui/home/HomeFragment.kt
@@ -7,15 +7,18 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import jp.osdn.gokigen.aira01c.R
 import jp.osdn.gokigen.aira01c.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment()
+{
 
     private var _binding: FragmentHomeBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,16 +30,15 @@ class HomeFragment : Fragment() {
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
         val textView: TextView = binding.textHome
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
-    }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+
+
+       // val root: View = inflater.inflate(R.layout.fragment_home, container, false)
+       // return root
     }
 }
