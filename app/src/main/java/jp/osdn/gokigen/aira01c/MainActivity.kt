1700e1e8c1846c908ff1cfc4838a46c8219bfa7d
MRSa <mrsa@myad.jp>
2024-08-04 10:07:08 +0900

    Tips集のレイアウトを調整。

--- app/src/main/java/jp/osdn/gokigen/aira01c/MainActivity.kt
+++ app/src/main/java/jp/osdn/gokigen/aira01c/MainActivity.kt
@@ -6,12 +6,16 @@ import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
