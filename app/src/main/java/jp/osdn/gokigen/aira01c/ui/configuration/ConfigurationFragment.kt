0b91d91486e38a7729bf6a7964598ba22c030254
MRSa <mrsa@myad.jp>
2024-08-17 19:53:51 +0900

    レイアウトを微調整。

--- app/src/main/java/jp/osdn/gokigen/aira01c/ui/configuration/ConfigurationFragment.kt
+++ app/src/main/java/jp/osdn/gokigen/aira01c/ui/configuration/ConfigurationFragment.kt
@@ -1,7 +1,7 @@
 package jp.osdn.gokigen.aira01c.ui.configuration
 
-import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
@@ -58,8 +58,6 @@ class ConfigurationFragment : Fragment()
         }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 
-
-    @SuppressLint("SetTextI18n")
     private fun setupOnClickListener(view: View)
     {
         try
@@ -85,14 +83,15 @@ class ConfigurationFragment : Fragment()
             view.findViewById<Button>(R.id.btnStandalone12)?.setOnClickListener(onClickListener)
             view.findViewById<Button>(R.id.btnStandalone13)?.setOnClickListener(onClickListener)
 
-            view.findViewById<TextView>(R.id.command_response_area)?.text = ""
+            view.findViewById<TextView>(R.id.command_response_area)?.text = requireActivity().getString(R.string.blank)
+
+            Log.v(TAG, "setupOnClickListener() done.")
         }
         catch (e: Exception)
         {
             e.printStackTrace()
         }
     }
-
 
     companion object
     {
