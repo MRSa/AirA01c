0683661ae8e00fbabee2d3b914a9048a5bbe5e97
MRSa <mrsa@myad.jp>
2024-08-17 21:12:42 +0900

    次バージョンに向けてバージョン番号更新。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "jp.osdn.gokigen.aira01c"
         minSdk = 14
         targetSdk = 35
-        versionCode = 100100
-        versionName = "1.1.0"
+        versionCode = 100101
+        versionName = "1.1.1"
         multiDexEnabled = true
     }
 
